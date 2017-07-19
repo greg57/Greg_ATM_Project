8bd2546ae0dc9f2818b6bec7e3164b50c94a06da
Greg Guy <javag57@gmail.com>
2017-07-19 13:40:03 +0200

    Committed 19/7/2017 13:40pm

--- src.main.java/vzap/greg/server/VZAP_BankServer.java
+++ src.main.java/vzap/greg/server/VZAP_BankServer.java
@@ -9,7 +9,6 @@ import vzap.greg.dao.*;
 import vzap.greg.dto.ATM_ServerDTO;
 import vzap.greg.exception.*;
 
-
 @SuppressWarnings("all")
 public class VZAP_BankServer
 {
@@ -22,7 +21,7 @@ public class VZAP_BankServer
 
 	public VZAP_BankServer()
 	{
-		Properties serverProperties  = new Properties();
+		Properties serverProperties = new Properties();
 		try
 		{
 			FileInputStream in = new FileInputStream("resources/atmServer.properties");
@@ -41,9 +40,9 @@ public class VZAP_BankServer
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		runServer = true;
-		//listeningPort = 10000;
+		// listeningPort = 10000;
 		try
 		{
 			serverSocket = new ServerSocket(listeningPort);
@@ -87,11 +86,9 @@ public class VZAP_BankServer
 			{
 				bankCardDAO = new BankCardMySQL_DAO_Impl();
 				clientDAO = new BankClientDAO_Impl();
-				outputToClient = new ObjectOutputStream(
-						socketFromAccept.getOutputStream());
-				inputFromClient = new ObjectInputStream(
-						socketFromAccept.getInputStream());
-				
+				outputToClient = new ObjectOutputStream(socketFromAccept.getOutputStream());
+				inputFromClient = new ObjectInputStream(socketFromAccept.getInputStream());
+
 			}
 			catch (Exception e)
 			{
@@ -105,40 +102,45 @@ public class VZAP_BankServer
 			// Receive a card object from the client
 			try
 			{
-				System.out.println("got connection from client: "
-						+ socketFromAccept.toString());
-				dto = (ATM_ServerDTO)inputFromClient.readObject();
-				messageFromClient = dto.getMessageToServer();
-				System.out.println("<<<<...Message From Client...>>>> : " + messageFromClient);
-				BankCard card = dto.getBankClient().getBankCard();
-				System.out.println("Bank Card details Card No: "
-						+ card.getCardNumber() + " Pin code: "
-						+ card.getPinNumber());
-				Properties atmProperties = dto.getAtmProperties();
-				System.out.println("ATM properties = " + atmProperties.toString());
-				BankCard validBankCard = bankCardDAO.validateBankCard(card);
-				String messageToClient = null;
-				if (validBankCard != null)
+				while (true)
 				{
-					messageToClient = new String("Card Accepted...>>>");
-					System.out.println("Validated card...>>>>>>");
-					bankClient = clientDAO.searchForBankClient(validBankCard);
-					System.out.println("In server: \n" + bankClient.toString());
+					System.out.println("got connection from client: " + socketFromAccept.toString());
+					dto = (ATM_ServerDTO) inputFromClient.readObject();
+					messageFromClient = dto.getMessageToServer();
+					System.out.println("<<<<...Message From Client...>>>> : " + messageFromClient);
+					if(messageFromClient.equals("end"))
+					{
+						break;
+					}
+					BankCard card = dto.getBankClient().getBankCard();
+					System.out.println(
+							"Bank Card details Card No: " + card.getCardNumber() + " Pin code: " + card.getPinNumber());
+					Properties atmProperties = dto.getAtmProperties();
+					System.out.println("ATM properties = " + atmProperties.toString());
+					BankCard validBankCard = bankCardDAO.validateBankCard(card);
+					String messageToClient = null;
+					if (validBankCard != null)
+					{
+						messageToClient = new String("Card Accepted...>>>");
+						System.out.println("Validated card...>>>>>>");
+						bankClient = clientDAO.searchForBankClient(validBankCard);
+						System.out.println("In server: \n" + bankClient.toString());
+					}
+					else
+					{
+						messageToClient = new String("Card REJECTED...>>>");
+						System.out.println("Card rejected...>>>>>>");
+						bankClient = null;
+					}
+					outputToClient.writeObject(messageToClient);
+					outputToClient.flush();
+					outputToClient.writeObject(bankClient);
+					outputToClient.flush();
 				}
-				else
-				{
-					messageToClient = new String("Card REJECTED...>>>");
-					System.out.println("Card rejected...>>>>>>");
-					bankClient = null;
-				}
-				outputToClient.writeObject(messageToClient);
-				outputToClient.flush();
-				outputToClient.writeObject(bankClient);
-				outputToClient.flush();
-
 				outputToClient.close();
 				inputFromClient.close();
 				socketFromAccept.close();
+				System.out.println("Socket closed with ATM...>>>>>>");
 
 			}
 			catch (Exception e)
